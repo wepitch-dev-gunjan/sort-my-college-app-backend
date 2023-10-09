c8a07034d8c738ae65621f0a5a634573604018fc
Nmn200 <n9462895233@gmail.com>
2023-10-09 13:35:47 +0530

    more email notifications added

--- notification-service/routes/emailNotificationRoutes.js
+++ notification-service/routes/emailNotificationRoutes.js
@@ -8,6 +8,11 @@ const {
   reminderSessionCounsellorEmailNotification,
   attendedSessionUserEmailNotification,
   attendedSessionCounsellorEmailNotification,
+  notattendedSessionCounsellorEmailNotification,
+  notattendedSessionUserEmailNotification,
+  cancelledSessionUserEmailNotification,
+  cancelledSessionCounsellorEmailNotification,
+  gotreviewSessionCounsellorEmailNotification,
 } = require("../controllers/emailNotificationControllers");
 const router = express.Router();
 
@@ -15,6 +20,8 @@ router.post("/user/welcome", welcomeUserEmailNotification);
 router.post("/user/sessionbooked", bookedSessionUserEmailNotification);
 router.post("/user/reminder", reminderSessionUserEmailNotification);
 router.post("/user/attended/", attendedSessionUserEmailNotification);
+router.post("/user/notattended/", notattendedSessionUserEmailNotification);
+router.post("/user/cancelled/", cancelledSessionUserEmailNotification);
 
 router.post("/counsellor/welcome", welcomeCounsellorEmailNotification);
 router.post(
@@ -26,4 +33,17 @@ router.post(
   "/counsellor/attended/",
   attendedSessionCounsellorEmailNotification
 );
+router.post(
+  "/counsellor/notattended/",
+  notattendedSessionCounsellorEmailNotification
+);
+router.post(
+  "/counsellor/cancelled/",
+  cancelledSessionCounsellorEmailNotification
+);
+router.post(
+  "/counsellor/gotreview/",
+  gotreviewSessionCounsellorEmailNotification
+);
+
 module.exports = router;
