8a19a12e9d7d6c0288d26a4d25d4e2809d5e6dae
gunjan <gunjan.soral@gmail.com>
2023-10-10 14:31:04 +0530

    message notification service added

---
+++ notification-service/routes/whatsappNotificationRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { sendWhatsappMessage, sendSMSMessage } = require('../controllers/whatsappNotificationControllers');
+const router = express.Router();
+
+router.post('/whatsapp', sendWhatsappMessage);
+router.post('/sms', sendSMSMessage);
+
+module.exports = router;
