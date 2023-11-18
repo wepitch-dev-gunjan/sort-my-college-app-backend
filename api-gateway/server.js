75367b0f903eccc047de3cb5ba56aa45c6f90eda
gunjan <gunjan.soral@gmail.com>
2023-11-18 16:06:01 +0530

    ssl implemented

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,7 +1,24 @@
-const gateway = require('fast-gateway')
+const gateway = require('fast-gateway');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 
-const { PORT, USER_PORT, COUNSELLOR_PORT, ENTRANCE_PREPARATIONS_PORT, VOCATIONAL_COURSES_PORT, WEBINARS_PORT, NOTIFICATION_SERVICES_PORT } = process.env;
+const {
+  PORT,
+  USER_PORT,
+  COUNSELLOR_PORT,
+  ENTRANCE_PREPARATIONS_PORT,
+  VOCATIONAL_COURSES_PORT,
+  WEBINARS_PORT,
+  NOTIFICATION_SERVICES_PORT
+} = process.env;
+
+const serverOptions = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt'),
+  ca: fs.readFileSync('ca_bundle.crt')
+};
+
 const server = gateway({
   routes: [
     {
@@ -29,12 +46,14 @@ const server = gateway({
       target: `http://127.0.0.1:${NOTIFICATION_SERVICES_PORT}`
     },
   ]
-})
+});
 
 server.get('/', (req, res) => {
-  res.send('welcome')
-})
+  res.send('welcome');
+});
 
-server.start(PORT).then(() => {
-  console.log('server started at port: ' + PORT);
-})
+const httpsServer = https.createServer(serverOptions, server.app);
+
+httpsServer.listen(PORT, () => {
+  console.log('HTTPS server started at port: ' + PORT);
+});
