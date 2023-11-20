8ff62046d228c928e264acd9ba970e9530665171
gunjan <gunjan.soral@gmail.com>
2023-11-20 13:54:05 +0530

    kkkk

--- counselor/server.js
+++ counselor/server.js
@@ -6,20 +6,17 @@ const cors = require("cors");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sort-my-college";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sort-my-college";
 
 // Parse URL-encoded form data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Configure CORS
-const corsOptions = {
-  origin: "*",
-  credentials: true, // Allow cookies to be sent cross-origin
-};
-
-app.use(cors(corsOptions));
+// CORS configuration
+app.use(cors({
+  origin: "https://counsellor.sortmycollege.com",
+  credentials: true,
+}));
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
@@ -33,6 +30,15 @@ mongoose.connection.on("connected", () => {
 
 mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
+});
+
+// Middleware to set CORS headers and allow credentials
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://counsellor.sortmycollege.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
 // Routes
