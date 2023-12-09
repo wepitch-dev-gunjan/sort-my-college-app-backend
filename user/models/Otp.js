0c337bf952d191367e2affca46637f873193072d
gunjan <gunjan.soral@gmail.com>
2023-12-09 17:46:29 +0530

    jgj

--- user/models/Otp.js
+++ user/models/Otp.js
@@ -4,31 +4,46 @@ const otpSchema = new Schema({
   phone_number: {
     type: Number,
     unique: true,
-    sparse: true,
-    default: null
+    sparse: true, // Allow multiple documents that have the same null or undefined value
   },
   email: {
     type: String,
     unique: true,
-    sparse: true,
-    default: null
+    sparse: true, // Allow multiple documents that have the same null or undefined value
   },
   hashedOtp: {
     type: String,
     required: true,
   },
   expiresAt: {
-    type: Date, // Store the expiration time
+    type: Date,
     required: true,
   },
   attempts: {
-    type: Number, // Store the number of verification attempts
+    type: Number,
     default: 0,
   },
 }, {
-  timestamps: true
+  timestamps: true,
 }, {
-  strict: false
+  strict: false,
 });
+
+// Custom validation to check uniqueness for non-null values
+otpSchema.path('phone_number').validate(async function (value) {
+  if (value !== null) {
+    const count = await this.model('Otp').countDocuments({ phone_number: value, _id: { $ne: this._id } });
+    return !count;
+  }
+  return true;
+}, 'Phone number must be unique except for null values.');
+
+otpSchema.path('email').validate(async function (value) {
+  if (value !== null) {
+    const count = await this.model('Otp').countDocuments({ email: value, _id: { $ne: this._id } });
+    return !count;
+  }
+  return true;
+}, 'Email must be unique except for null values.');
 
 module.exports = model('Otp', otpSchema);
