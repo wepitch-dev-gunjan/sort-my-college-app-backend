ae578d017489bec62c2565d212170c84fbe01152
gunjan <gunjan.soral@gmail.com>
2023-12-09 15:49:35 +0530

    hghghggh

--- user/services/authService.js
+++ user/services/authService.js
@@ -215,7 +215,7 @@ exports.verifyOtpByEmail = async (req, res) => {
       await user.save();
     }
     const { _id, phone_number } = user;
-    const token = jwt.sign({ _id, phone_number }, JWT_SECRET)
+    const token = jwt.sign({ _id, email }, JWT_SECRET)
     const { data } = await axios.post(`${BACKEND_URL}/notification/verifiedOtp`, {
       to: email,
     })
