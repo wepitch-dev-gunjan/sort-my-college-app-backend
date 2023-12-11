47a61c3cdf7250c21a6c0790427ae484d158fa5d
gunjan <gunjan.soral@gmail.com>
2023-12-11 12:27:08 +0530

    hghgh

--- counselor/routes/feedbackRoutes.js
+++ counselor/routes/feedbackRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createFeedback, getFeedbacks, getFeedback, editFeedback, deleteFeedback } = require('../controllers/feedbackController');
+const { userAuth, counsellorAuth } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/feedback', createFeedback);
-router.get('/feedback/getall', getFeedbacks);
+router.post('/feedback', userAuth, createFeedback);
+router.get('/feedback/getall', counsellorAuth, getFeedbacks);
 router.get('/feedback/:feedback_id', getFeedback);
 router.put('/feedback/:feedback_id', editFeedback);
 router.delete('/feedback/:feedback_id', deleteFeedback);
