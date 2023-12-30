105f55c9149a98c4516da3b1ee93dae06a81c6f6
Nmn200 <n9462895233@gmail.com>
2023-12-30 15:09:44 +0530

    home page banner

--- admin/controllers/homePageBannerController.js
+++ admin/controllers/homePageBannerController.js
@@ -1,8 +1,42 @@
-exports.createBanner = (req, res) => {
+const HomePageBanner = require("../models/homePageBanner");
+
+exports.createBanner = async (req, res) => {
   try {
-    { }
+    const { name, url } = req.body;
+
+    const newBanner = new HomePageBanner({
+      name,
+      url,
+    });
+
+    const data = await newBanner.save();
+
+    res.status(200).send({
+      message: "Banner successfully created",
+      data,
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ error: 'Internal Server Error' });
+    res.status(500).send({ error: "Internal server error" });
+  }
+};
+
+exports.deleteBanner = async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    const banner = await HomePageBanner.findByIdAndDelete(_id);
+
+    if (!banner) res.status(400).send({ message: "Banner not found" });
+
+    res.status(200).send({ message: "Banner deleted successfully" });
+
+    res.status(200).send({
+      message: "Banner successfully deleted",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Internal server error" });
   }
 };
--- admin/routes/homePageBannerRoutes.js
+++ admin/routes/homePageBannerRoutes.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const { createBanner } = require('../controllers/homePageBannerController');
+const express = require("express");
+const {
+  createBanner,
+  deleteBanner,
+} = require("../controllers/homePageBannerController");
 const router = express.Router();
 
-router.post('/home-page-banner', createBanner);
+router.post("/home-page-banner", createBanner);
+router.delete("/home-page-banner/:_id", deleteBanner);
 
 module.exports = router;
