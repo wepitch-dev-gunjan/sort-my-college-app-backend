5d19bfdf13e34c5cecc84119b437d259cc729ff6
gunjan <gunjan.soral@gmail.com>
2024-02-22 17:10:06 +0530

    telesign api integration done

--- notification-service/controllers/smsNotificationController.js
+++ notification-service/controllers/smsNotificationController.js
@@ -1,18 +1,9 @@
-const sdk = require('api')('@msg91api/v5.0#6n91xmlhu4pcnz');
-require('dotenv').config();
-const { MSG91_AUTH_KEY } = process.env;
+const { client, messageType, smsCallback } = require('../services/smsService');
 
-sdk.auth(MSG91_AUTH_KEY);
-console.log(sdk.auth(MSG91_AUTH_KEY))
 exports.sendSMS = async (req, res) => {
   try {
     const { to, message } = req.body;
-    const sms = await sdk.sendSms({
-      template_id: "658bf380d6fc057df8195742",
-      short_url: '1', // Adjust this based on your preference
-      recipients: [{ mobiles: [to], otp: message }] // Adjust the recipients structure
-    });
-    console.log(sms); // Log the response for debugging purposes
+    await client.sms.message(smsCallback, to, message, messageType);
     res.status(200).send({ message: "Message sent successfully" });
   } catch (error) {
     console.log(error);
