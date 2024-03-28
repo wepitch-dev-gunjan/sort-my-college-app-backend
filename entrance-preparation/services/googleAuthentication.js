e76fd48ddb6d87f7ee020449a0cf203edbd70004
gunjan <gunjan.soral@gmail.com>
2024-03-28 14:37:44 +0530

    webinar bug resolved

--- entrance-preparation/services/googleAuthentication.js
+++ entrance-preparation/services/googleAuthentication.js
@@ -17,7 +17,6 @@ router.get('/auth/google', (req, res) => {
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/calendar'
     ],
   });
 
