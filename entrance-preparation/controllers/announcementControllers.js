e885322dcef892308369a2c30671698fc9cbc689
Rakshitaaa <rakshitakanwar09@gmail.com>
2024-04-08 17:56:55 +0530

    finl for today

--- entrance-preparation/controllers/announcementControllers.js
+++ entrance-preparation/controllers/announcementControllers.js
@@ -41,7 +41,33 @@ exports.addAnnouncements = async (req, res) => {
         res.status(201).json({ message: "Announcement added successfully", data: newAnnouncement});
 
     } catch (error) {
-        console.error("Error adding Key Announcements: ", error);
+        console.error("Error adding Announcements: ", error);
+        res.status(500).json({ message: "Internal Server Error!!" });
+    }
+}
+
+exports.deleteAnnouncement = async (req, res) => {
+    try {
+        const { institute_id } = req;
+        const { announcement_id } = req.params;
+
+        if (!institute_id) {
+            return res.status(404).json({ message: "Institute Id not found!" });
+        }
+        if (!announcement_id) {
+            return res.status(404).json({ message: "Announcement Id not found!" });
+        }
+
+        const announcement = await Announcement.findOneAndDelete({ _id: announcement_id, institute: institute_id });
+
+        if (!announcement) {
+            return res.status(404).json({ message: "Announcement not found!" });
+        }
+
+        res.status(200).json({ message: "Announcement deleted successfully", data: announcement });
+
+    } catch (error) {
+        console.error("Error deleting Announcement: ", error);
         res.status(500).json({ message: "Internal Server Error!!" });
     }
 }
--- entrance-preparation/routes/announcementRoutes.js
+++ entrance-preparation/routes/announcementRoutes.js
@@ -7,12 +7,14 @@ const {
 
 const {
     getAnnouncements,
-    addAnnouncements
+    addAnnouncements,
+    deleteAnnouncement
 } = require("../controllers/announcementControllers")
 const router = express.Router();
 
 // EP Panel Routes
 router.get("/announcements", epAuth, getAnnouncements);
 router.post("/announcements", epAuth, addAnnouncements);
+router.delete("/announcements/:announcement_id", epAuth, deleteAnnouncement)
 
 module.exports = router;
