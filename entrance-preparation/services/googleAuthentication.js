e7e50394771efd2ab37a0ffd34c13d153e357dff
Nikhil mandia <nikhilmandia7@gmail.com>
2024-04-30 11:10:03 +0530

    token expair time removed

--- entrance-preparation/services/googleAuthentication.js
+++ entrance-preparation/services/googleAuthentication.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const { google } = require('googleapis');
-const { FRONTEND_URL, OAUTH2_CLIENT_ID, OAUTH2_CLIENT_SECRET, OAUTH2_REDIRECT_URI } = process.env;
-const { generateToken } = require('../helpers/instituteHelpers');
-const EntranceInstitute = require('../models/EntranceInstitute');
+const express = require("express");
+const { google } = require("googleapis");
+const {
+  FRONTEND_URL,
+  OAUTH2_CLIENT_ID,
+  OAUTH2_CLIENT_SECRET,
+  OAUTH2_REDIRECT_URI,
+} = process.env;
+const { generateToken } = require("../helpers/instituteHelpers");
+const EntranceInstitute = require("../models/EntranceInstitute");
 
 const router = express.Router();
 
 // Initialize the Google OAuth2 client (You can move this to your main app file if needed)
-const oauth2Client = new google.auth.OAuth2(OAUTH2_CLIENT_ID, OAUTH2_CLIENT_SECRET, OAUTH2_REDIRECT_URI);
+const oauth2Client = new google.auth.OAuth2(
+  OAUTH2_CLIENT_ID,
+  OAUTH2_CLIENT_SECRET,
+  OAUTH2_REDIRECT_URI
+);
 
 // Route for initiating Google OAuth2 authentication
-router.get('/auth/google', (req, res) => {
+router.get("/auth/google", (req, res) => {
   const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    prompt: 'consent',
+    access_type: "offline",
+    prompt: "consent",
     scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
     ],
   });
 
@@ -24,13 +33,15 @@ router.get('/auth/google', (req, res) => {
 });
 
 // Route to handle the Google OAuth2 callback
-router.get('/auth/google/callback', async (req, res) => {
+router.get("/auth/google/callback", async (req, res) => {
   const { code } = req.query;
   try {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
 
-    const { data } = await google.oauth2('v2').userinfo.get({ auth: oauth2Client });
+    const { data } = await google
+      .oauth2("v2")
+      .userinfo.get({ auth: oauth2Client });
     let { email, name, picture } = data;
 
     // Save user information to the database if not already exists
@@ -49,19 +60,21 @@ router.get('/auth/google/callback', async (req, res) => {
       email: institute.email,
       name: institute.name,
       picture: institute.profile_pic,
-      tokens
-    }, '7d');
+      tokens,
+    });
 
     const user = {
       _id: institute._id,
       email: institute.email,
       name: institute.name,
       // profile_pic: counsellor.profile_pic
-    }
+    };
 
-    const redirectURL = `${FRONTEND_URL}/?token=${token}&&user=${JSON.stringify(user)}`;
+    const redirectURL = `${FRONTEND_URL}/?token=${token}&&user=${JSON.stringify(
+      user
+    )}`;
 
-    console.log(redirectURL)
+    console.log(redirectURL);
     res.redirect(redirectURL);
   } catch (error) {
     console.error(error);
--- user/services/googleAuthentication.js
+++ user/services/googleAuthentication.js
@@ -1,21 +1,29 @@
-
-const express = require('express');
-const { google } = require('googleapis');
-const { FRONTEND_URL, OAUTH2_CLIENT_ID, OAUTH2_CLIENT_SECRET, OAUTH2_REDIRECT_URI } = process.env;
-const { generateToken } = require('../helpers/userHelpers');
-const User = require('../models/User');
+const express = require("express");
+const { google } = require("googleapis");
+const {
+  FRONTEND_URL,
+  OAUTH2_CLIENT_ID,
+  OAUTH2_CLIENT_SECRET,
+  OAUTH2_REDIRECT_URI,
+} = process.env;
+const { generateToken } = require("../helpers/userHelpers");
+const User = require("../models/User");
 
 const router = express.Router();
 
-const oauth2Client = new google.auth.OAuth2(OAUTH2_CLIENT_ID, OAUTH2_CLIENT_SECRET, OAUTH2_REDIRECT_URI);
+const oauth2Client = new google.auth.OAuth2(
+  OAUTH2_CLIENT_ID,
+  OAUTH2_CLIENT_SECRET,
+  OAUTH2_REDIRECT_URI
+);
 
 // Route for initiating Google OAuth2 authentication
-router.get('/auth/google', (req, res) => {
+router.get("/auth/google", (req, res) => {
   const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: "offline",
     scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
     ],
   });
 
@@ -23,14 +31,16 @@ router.get('/auth/google', (req, res) => {
 });
 
 // Route to handle the Google OAuth2 callback
-router.get('/auth/google/callback', async (req, res) => {
+router.get("/auth/google/callback", async (req, res) => {
   const { code } = req.query;
   try {
     // Assuming you have previously set up oauth2Client
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
 
-    const userInfo = await google.oauth2('v2').userinfo.get({ auth: oauth2Client });
+    const userInfo = await google
+      .oauth2("v2")
+      .userinfo.get({ auth: oauth2Client });
     const { email, name, picture } = userInfo.data;
 
     // Save user information to the database if not already exists
@@ -46,9 +56,9 @@ router.get('/auth/google/callback', async (req, res) => {
       await user.save();
     }
 
-    const token = generateToken({ email }, '7d');
+    const token = generateToken({ email }, "7d");
     // Redirect to homepage or dashboard
-    res.cookie('token', token, { maxAge: 7 * 24 * 60 * 60 * 1000 });
+    res.cookie("token", token, { maxAge: 7 * 24 * 60 * 60 * 1000 });
     res.redirect(`${FRONTEND_URL}/`);
   } catch (error) {
     console.error(error);
