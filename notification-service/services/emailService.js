89bfc88862165802125f0e7e65a18b7c50ea6238
Nikhil mandia <nikhilmandia7@gmail.com>
2024-05-28 14:39:25 +0530

    afsdf

--- notification-service/services/emailService.js
+++ notification-service/services/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 exports.transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "support.sortmycollege.com",
+    user: "smcapp.official@gmail.com",
     pass: "cqds sbpg neqe lakd",
   },
 });
