c4d620362ca6fd8ca0f93b5ef87a212f6080bb20
Nikhil mandia <nikhilmandia7@gmail.com>
2024-05-29 11:23:45 +0530

    session validation change

---
+++ admin/models/AccommodationEnquiry.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const accommodationEnquirySchema = new Schema(
+  {
+    enquirer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    preferred_time: [String],
+    enquired_to: {
+      type: Schema.Types.ObjectId,
+      ref: "Accommodation",
+    },
+    enquiryStatus: {
+      type: String,
+      enum: ["Unseen", "Pending", "Sent"],
+      default: "Unseen",
+    },
+  },
+  {
+    timestamps: true,
+    strict: false,
+  }
+);
+
+module.exports = model("Enquiry", enquirySchema);
