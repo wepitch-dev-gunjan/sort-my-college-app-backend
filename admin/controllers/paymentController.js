144d50b8cac6c06cf55a27b032e557b679d1efe6
piyush <piyushpk73523@gmail.com>
2024-09-18 16:19:14 +0530

    testing11

--- admin/controllers/paymentController.js
+++ admin/controllers/paymentController.js
@@ -311,7 +311,7 @@ exports.incomeofcounsellor = async (req, res) => {
 
     const result = await Payment.aggregate([
       { $match: { payment_to: counsellor_id } },
-      { $group: { _id: "$payment_to", totalIncome: { $sum: "$fees_amount" } } },
+      { $group: { _id: "$payment_to", totalIncome: { $sum: "$amount_due" } } },
     ]);
 
     const totalIncome = result.length > 0 ? result[0].totalIncome : 0;
